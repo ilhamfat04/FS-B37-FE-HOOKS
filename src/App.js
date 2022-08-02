a3e57005dcc2365d55225928e6adfc51c36665b4
ilham fathullah <ilhamfathullah04@gmail.com>
2022-08-02 15:01:14 +0700

    update

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import Welcome from './Welcome'
 import GuestGreeting from './GuestGreeting'
 
 import ExampleComp from './ExampleComp'
+import List from './component/List'
 
 import { UserContext } from './context/userContext'
 
@@ -50,41 +51,7 @@ function App() {
 
     return (
         <>
-            <ExampleComp user={ state.user } />
-            { state.isLogin ? <Welcome /> :
-                (<>
-                    <GuestGreeting />
-                    <Container>
-                        <Row
-                            className="d-flex justify-content-center mt-5">
-                            <Col md="4">
-                                <Form onSubmit={ handleOnSubmit }>
-                                    <div className="text-center h5">Login</div>
-
-                                    <Form.Group className="mb-3" controlId="formBasicEmail">
-                                        <Form.Label>Email address</Form.Label>
-                                        <Form.Control
-                                            id="email"
-                                            name="email" size="sm" type="email"
-                                            placeholder="Enter email" />
-                                    </Form.Group>
-
-                                    <Form.Group className="mb-3" controlId="formBasicPassword">
-                                        <Form.Label>Password</Form.Label>
-                                        <Form.Control
-                                            id="password"
-                                            name="password" size="sm" type="password"
-                                            placeholder="Password" />
-                                    </Form.Group>
-
-                                    <Button variant="primary" type="submit" size="sm">
-                                        Submit
-                                    </Button>
-                                </Form>
-                            </Col>
-                        </Row>
-                    </Container>
-                </>) }
+            <List />
         </>
     )
 }
---
+++ src/fakeData/contentData.js
@@ -0,0 +1,25 @@
+const contentData = [
+    {
+        id: 1,
+        name: 'mercedez benz',
+        image: 'https://images.unsplash.com/photo-1533473359331-0135ef1b58bf?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8Y2FyfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60'
+    },
+    {
+        id: 2,
+        name: 'bmw',
+        image: 'https://images.unsplash.com/photo-1533473359331-0135ef1b58bf?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8Y2FyfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60'
+    },
+
+    {
+        id: 3,
+        name: 'carry',
+        image: 'https://images.unsplash.com/photo-1533473359331-0135ef1b58bf?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8Y2FyfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60'
+    },
+    {
+        id: 4,
+        name: 'carry',
+        image: 'https://images.unsplash.com/photo-1533473359331-0135ef1b58bf?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8Y2FyfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60'
+    },
+]
+
+export default contentData
